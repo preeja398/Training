901aa48e205b0a28e0f8346237c91aa097ed0500
31673 <kalaiyarasan.k@tataelxsi.co.in>
2022-12-05 18:00:33 +0530

    changed format of scope_space before and after operators_added fp(close)_aligned the code in proper formate

--- filehandling.c
+++ filehandling.c
@@ -1,48 +1,41 @@
 #include<stdio.h>
 
-struct employee
-{
+struct Employee{
 	char employeeName[50];
 	int employeeId;
 };
 
-int main()
-{
-	FILE *fp=NULL;
-	struct employee emp[2];
-	int i;
-	for(i=0;i<2;i++)
-{
-	printf("Enter the employee details\n");
-	printf("Name:");
-	scanf("%s",emp[i].employeeName);
-	printf("Id:");
-	scanf("%d",&emp[i].employeeId);
-}
+int main(){
+    FILE *fp = NULL;
+    struct Employee emp[2];
+    int index;
+    for(index = 0; index < 2; index ++){
+	    printf("Enter the employee details\n");
+	    printf("Name:");
+	    scanf("%s", emp[index].employeeName);
+	    printf("Id:");
+	    scanf("%d", &emp[index].employeeId);
+    }
+    
+    fp = fopen("file.bin", "wb");
+    if(fp == NULL) {
+	    printf("error in open file in write mode");
+    } else {
+	    for(index = 0; index < 2; index ++){
+		    fwrite(&emp[index], sizeof(struct Employee), 1, fp);
+	    }
+	    fclose(fp);
+    }
 
-fp=fopen("file.bin","wb");
-if(fp==NULL)
-{
-	printf("error in open file in write mode");
-}
-else
-{
-	for(i=0;i<2;i++)
- fwrite(&emp[i],sizeof(struct employee),1,fp);
-}
-
-fp=fopen("file.bin","rb");
-{
-	if(fp==NULL)
-	printf("error open filn in read mode");
-else
-{
-for(i=0;i<2;i++)
-{
-fread(&emp[i],sizeof(struct employee),1,fp);
-printf("Employee name:%s\tEmployee id:%d\n",emp[i].employeeName,emp[i].employeeId);
-}
-}
-}
+    fp = fopen("file.bin", "rb");
+    if(fp == NULL) {
+	    printf("error open filn in read mode");
+    } else {
+	    for(index = 0; index < 2; index ++) {
+		    fread(&emp[index], sizeof(struct Employee), 1, fp);
+		    printf("Employee name:%s\tEmployee id:%d\n", emp[index].employeeName, emp[index].employeeId);
+	    }
+	    fclose(fp);
+    }
 }
 
