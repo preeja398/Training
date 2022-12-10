2d4d03d895a756f65ec52f19fcdb969522e223c9
31673 <kalaiyarasan.k@tataelxsi.co.in>
2022-12-10 21:41:43 +0530

    created nem program for sum of element in a Dynamically allocated array

---
+++ sum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int number, *ptr, index, sum = 0;
+    printf("Enter the number of elements:");
+    scanf("%d", &number);
+    ptr = (int *)malloc(number * sizeof(int));
+
+    if (ptr == NULL) {
+	    printf("Memory not allocated");
+	    exit(1);
+    } else {
+
+    for(index = 0; index < number; index ++) {
+	    printf("Enter the %d element:", index + 1);
+	    scanf("%d", &ptr[index]);
+ 	    sum = sum + ptr[index];
+    }
+    printf("Sum of Elements:%d", sum);
+    }
+}
+
