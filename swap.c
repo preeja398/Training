5fbbe7e5d1c21c32f3ea29fadedf2d6b5d7d251a
31673 <kalaiyarasan.k@tataelxsi.co.in>
2022-12-10 20:46:57 +0530

    Created new swap element program

---
+++ swap.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void callByValue(int a, int b) {
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+void callByReference(int *a, int *b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main() {
+    int a, b;
+    printf("Enter the element of a and b:");
+    scanf("%d%d", &a, &b);
+    printf("Before swap elements:a = %d b = %d\n", a, b);
+    callByValue(a, b);
+    printf("After swap by call by value a = %d b = %d\n", a, b);
+    callByReference(&a, &b);
+    printf("After swap by call by reference a = %d b = %d\n", a, b);
+}
+
+
