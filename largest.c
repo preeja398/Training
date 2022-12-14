ca6a6ca3a1de5d0d9cb71e94c81858157451ff3c
31671 <aswinsuresh.a@tataelxsi.co.in>
2022-12-14 20:57:35 +0530

    format changed

--- largest.c
+++ largest.c
@@ -2,26 +2,31 @@
 #include<stdlib.h>
 
 int main() {
-    int *num, index, N, MAX;
+    int *num, index, N, MAX = 0;
     printf("Enter the no of Elements :  ");
     scanf("%d", &N);
     num = (int *) malloc (N * sizeof(int));
 
-    for(index = 0; index < N; index ++) {
-            printf("Element %d : ", index + 1);
-            scanf("%d",&num[index]);
+    if(num == NULL) {
+	    printf("members not allocated");
+    } else {
+
+       for(index = 0; index < N; index ++) {
+	    printf("Element %d : ", index + 1);
+	    scanf("%d",&num[index]);
+       }
     }
-
-    MAX = num[0];
-
-    for(index = 1; index < N; index ++){
-            if( MAX < num[index]) {
-                    MAX = num[index];
-            }
-
+     
+    for(index = 0; index < N; index ++) {
+	   
+	    if( MAX < num[index]) {
+		    MAX = num[index];
+	    }
+    
     }
 
     printf("MAX : %d\n",MAX);
+    free(num);
 
 }
 
