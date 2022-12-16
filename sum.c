27dbce9a1094ebfcade3a306ed6ebf85187fd4e5
31673 <kalaiyarasan.k@tataelxsi.co.in>
2022-12-17 01:29:08 +0530

    Removed exit() in if statement and moved free() into else statement

---
+++ sum.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int number;
+    int *ptr;
+    int index;
+    int sum = 0;
+    printf("Enter the number of elements:\n");
+    scanf("%d", &number);
+    ptr = (int *)malloc(number * sizeof(int));
+
+    if (ptr == NULL) {
+        printf("Memory not allocated\n");
+    } else {
+	    
+          for (index = 0; index < number; index ++) {
+	     printf("Enter the %d element:\n", index + 1);
+	     scanf("%d", &ptr[index]);
+ 	     sum = sum + ptr[index];
+	  } 
+	  printf("Sum of Elements:%d\n", sum);
+	  free(ptr);
+    } 
+}
+
